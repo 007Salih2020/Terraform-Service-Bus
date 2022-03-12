d5578907fa2e756d44862cf7934b6f341d9033da
007Salih2020 <66966438+007Salih2020@users.noreply.github.com>
2022-03-12 17:08:14 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "client_id" {}
+variable "client_secret" {}
+ 
+variable "agent_count" {
+default = 3
+}
+ 
+ 
+variable resource_group_name {
+default = "azure-k8stest"
+}
+ 
+variable location {
+default = "Central US"
+}
+ 
+variable log_analytics_workspace_name {
+default = "testLogAnalyticsWorkspaceName"
+}
+ 
+# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
+variable log_analytics_workspace_location {
+default = "eastus"
+}
+ 
+# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing
+variable log_analytics_workspace_sku {
+default = "PerGB2018"
+}
