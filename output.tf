f81a45bdff9267e1b739b2c4ab203395ba33aa9a
007Salih2020 <66966438+007Salih2020@users.noreply.github.com>
2022-03-13 21:47:51 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "resource_group_name" {
     value = azurerm_resource_group.rg.name
 }
+
+output "log_analytics_workspace_id" {
+  value = data.azurerm_log_analytics_workspace.example.workspace_id
+}
